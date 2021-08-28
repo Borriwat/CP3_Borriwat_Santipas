1f2c325bdc18e22920b1251e60f21034a36682b7
Borriwat <borriwat.san@gmail.com>
2021-08-28 20:38:02 +0700

    Create Excercise4_Borriwat_S.py

---
+++ Excercise4_Borriwat_S.py
@@ -0,0 +1,11 @@
+eng = float(input("Foundation English score =" ))
+business = float(input("General Business score =" ))
+CS = float(input("Introduction to Computer Systems score =" ))
+programming = float(input("Computer Programming score =" ))
+
+print("--- Your Score ---")
+
+print("Foundation English : ", eng)
+print("General Business : ", business)
+print("Introduction to Computer Systems : ", CS)
+print("Computer Programming : ", programming)
